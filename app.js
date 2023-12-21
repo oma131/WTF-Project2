7f8797a12c474bc242f60173c5b64c810964f5be
oma131 <anosikeihuoma@gmail.com>
2023-12-21 09:21:35 +0100

    new commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const openForm = () => {
+    document.getElementById("my-form").style.display = "block";
+}
+
+const closeForm = () => {
+    document.getElementById("my-form").style.display = "none";
+}
